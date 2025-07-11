72af5ba9ebcef525cb46ad40a5ff205159e5d0f1
saniya <saniyagarg9@gmail.com>
2025-07-11 15:39:05 +0530

    put and fetch the data using addDoc and getDoc

--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import {getFirestore ,collection,addDoc,doc,getDoc} from "firebase/firestore"
+import {app} from './firebase'
 
+import './App.css';
+const firestore=getFirestore(app);
 function App() {
+  const writeData=async ()=>
+  {
+   const result=await  addDoc(collection(firestore,'cities'),
+  {
+    name:"Delhi",
+    pinCode:1234,
+    lat:123,
+    long:456
+  })
+  console.log("Result",result);
+  }
+  const makeSubCollections=async()=>{
+    await addDoc(collection(firestore,"cities/Dk7nvpEjbDM84B3BPITh/places"),{
+      name:"This is a place",
+      desc:"This is capital of India",
+      date:Date.now(),
+    })
+  }
+  const getDocument=async()=>
+  {
+    const ref=doc(firestore,"cities","Dk7nvpEjbDM84B3BPITh");
+    const snap=await getDoc(ref);
+    console.log(snap.data());
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Firebase Firestore</h1>
+      <button onClick={writeData}>Put Data</button>
+      <button onClick={makeSubCollections}>Put Sub data </button>
+      <button onClick={getDocument}>Get Document</button>
+     
     </div>
   );
 }
