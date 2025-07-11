22ea6858179e42826384871439822856a49a03d3
saniya <saniyagarg9@gmail.com>
2025-07-11 17:38:03 +0530

    learnt how to fetch the data through query and how to update already existing data

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import {getFirestore ,collection,addDoc,doc,getDoc} from "firebase/firestore"
+import {getFirestore ,collection,addDoc,doc,getDoc,query,where,getDocs,updateDoc} from "firebase/firestore"
 import {app} from './firebase'
 
 import './App.css';
@@ -28,12 +28,29 @@ function App() {
     const snap=await getDoc(ref);
     console.log(snap.data());
   }
+  const getDocumentByQuery=async()=>
+  {
+    const collectionRef=collection(firestore,"users");
+    const q=query(collectionRef,where("isMale","==",true));
+    const snap=await getDocs(q);
+    snap.forEach((data)=>console.log(data.data()));
+
+  }
+  const update=async()=>
+  {
+    const docRef=doc(firestore,'cities',"Dk7nvpEjbDM84B3BPITh");
+    await updateDoc(docRef,{
+      name:"hello",
+    })
+  }
   return (
     <div className="App">
       <h1>Firebase Firestore</h1>
       <button onClick={writeData}>Put Data</button>
       <button onClick={makeSubCollections}>Put Sub data </button>
       <button onClick={getDocument}>Get Document</button>
+      <button onClick={getDocumentByQuery}>Get Document By Query</button>
+      <button onClick={update}>Update</button>
      
     </div>
   );
